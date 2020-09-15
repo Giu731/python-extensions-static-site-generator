d6885b870bb7bea4395c49915d0abb1338365f6f
Giulia Bonadio Lisboa <63248417+Giu731@users.noreply.github.com>
2020-09-15 15:49:42 -0300

    Update menu.py

---
+++ ssg/extensions/menu.py
@@ -0,0 +1,11 @@
+from ssg import hooks, parsers
+
+files = []
+
+@hooks.register("collect_files")
+def collect_files(source, site_parsers):
+    valid = lambda p: not isinstance(p, parsers.ResourceParser)
+    for path in source.rglob("*")
+        for parser in list(filter(valid, site_parsers)):
+            if not path.suffix.parser.valid_file_ext():
+                files.append(path)
